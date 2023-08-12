8d3a851ca24afca5ea177f517967e0fffc61acbd
Jacek Kosinski <kashiash@gmail.com>
2023-08-12 21:41:38 +0200

    horizontal stack fixed

--- SwiftUICarousel/View/ContentView.swift
+++ SwiftUICarousel/View/ContentView.swift
@@ -7,10 +7,16 @@ struct ContentView: View {
     @State private var isCardTapped = false
 
     var body: some View {
-        HStack {
-            ForEach(sampleTrips.indices, id: \.self) { index in
-                TripCardView(destination: sampleTrips[index].destination, imageName: sampleTrips[index].image, isShowDetails: self.$isCardTapped)
+        GeometryReader { outerView in
+            HStack {
+                ForEach(sampleTrips.indices, id: \.self) { index in
+                    GeometryReader { innerView in
+                        TripCardView(destination: sampleTrips[index].destination, imageName: sampleTrips[index].image, isShowDetails: self.$isCardTapped)
+                    }
+                    .frame(width: outerView.size.width, height: 500)
+                }
             }
+            .frame(width: outerView.size.width, height: outerView.size.height)
         }
     }
 }
