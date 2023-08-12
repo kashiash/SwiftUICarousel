7d4a4ce9058198c0f610f0397aa85d4aaa1caec6
Jacek Kosinski <kashiash@gmail.com>
2023-08-12 21:56:14 +0200

    animation

--- SwiftUICarousel/View/ContentView.swift
+++ SwiftUICarousel/View/ContentView.swift
@@ -9,37 +9,41 @@ struct ContentView: View {
     @GestureState private var dragOffset: CGFloat = 0
 
     var body: some View {
-        GeometryReader { outerView in
-            HStack(spacing: 0) {
-                ForEach(sampleTrips.indices, id: \.self) { index in
-                    GeometryReader { innerView in
-                        TripCardView(destination: sampleTrips[index].destination, imageName: sampleTrips[index].image, isShowDetails: self.$isCardTapped)
+        ZStack {
+            GeometryReader { outerView in
+                HStack(spacing: 0) {
+                    ForEach(sampleTrips.indices, id: \.self) { index in
+                        GeometryReader { innerView in
+                            TripCardView(destination: sampleTrips[index].destination, imageName: sampleTrips[index].image, isShowDetails: self.$isCardTapped)
+                        }
+                        .padding(.horizontal, self.isCardTapped ? 0 : 20)
+                        .opacity(self.currentTripIndex == index ? 1.0 : 0.7)
+                        .frame(width: outerView.size.width, height: self.currentTripIndex == index ? (self.isCardTapped ? outerView.size.height : 450) : 400)
                     }
-                    //.frame(width: outerView.size.width, height: 500)
-                    .frame(width: outerView.size.width, height: self.currentTripIndex == index ? (self.isCardTapped ? outerView.size.height : 450) : 400)
                 }
+
+                .frame(width: outerView.size.width, height: outerView.size.height, alignment: .leading)
+                .offset(x: -CGFloat(self.currentTripIndex) * outerView.size.width)
+                .offset(x: self.dragOffset)
+                .gesture(
+                    !self.isCardTapped ?
+
+                    DragGesture()
+                        .updating(self.$dragOffset, body: { (value, state, transaction) in
+                            state = value.translation.width
+                        })
+                        .onEnded({ (value) in
+                            let threshold = outerView.size.width * 0.65
+                            var newIndex = Int(-value.translation.width / threshold) + self.currentTripIndex
+                            newIndex = min(max(newIndex, 0), sampleTrips.count - 1)
+
+                            self.currentTripIndex = newIndex
+                        })
+
+                    : nil
+                )
             }
-            .padding(.horizontal, self.isCardTapped ? 0 : 20)
-            .frame(width: outerView.size.width, height: outerView.size.height, alignment: .leading)
-            .offset(x: -CGFloat(self.currentTripIndex) * outerView.size.width)
-            .offset(x: self.dragOffset)
-            .gesture(
-                !self.isCardTapped ?
-
-                DragGesture()
-                    .updating(self.$dragOffset, body: { (value, state, transaction) in
-                        state = value.translation.width
-                    })
-                    .onEnded({ (value) in
-                        let threshold = outerView.size.width * 0.65
-                        var newIndex = Int(-value.translation.width / threshold) + self.currentTripIndex
-                        newIndex = min(max(newIndex, 0), sampleTrips.count - 1)
-
-                        self.currentTripIndex = newIndex
-                    })
-
-                : nil
-            )
+            .animation(.interpolatingSpring(mass: 0.6, stiffness: 100, damping: 10, initialVelocity: 0.3), value: dragOffset)
         }
         
     }
